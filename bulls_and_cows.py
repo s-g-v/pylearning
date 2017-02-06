e97edf14697dd0712db959d819cd6f55647f5726
Gennady Sikorskiy <g.sikorskiy@corp.mail.ru>
2017-02-06 13:57:32 +0300

    Make __doc__ string

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -24,23 +24,28 @@ def _user_input():
         if user_number.lower() in ("exit","quit","q"):
             print("Good buy!")
             sys.exit()
-        print("Please enter four-digit number, or 'exit' to finish game")
+        print("Please enter four-digit number, or 'exit', 'quit', 'q' to finish game")
         user_number = input()
     return user_number
 
 
-if __name__ == "__main__":
-    pc_number = _come_up_number()
-    print("""-=Bulls and cows game=-
+def play():
+    """-=Bulls and cows game=-
 PC comes up four-digit number. Try to quess it.
 After each step you will see response:
     First digit - how much digits from your variant is in PC's number.
     Second digit - how much of them is on the place.
-Good luck!""")
+Good luck!"""
+    print(play.__doc__)
+    pc_number = _come_up_number()
     print("Please enter four-digit number, or 'exit', 'quit', 'q' to finish game")
     result = ()
-    while result!=(4,4):
+    while result != (4, 4):
         user_number = _user_input()
         result = _match_input(pc_number, user_number)
         print(str(result))
     print("You are win!")
+
+
+if __name__ == "__main__":
+    play()
