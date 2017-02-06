9063075954523bbf201c532d8d601a1d77a73291
Gennady Sikorskiy <g.sikorskiy@corp.mail.ru>
2017-02-06 13:49:06 +0300

    py modules

---
+++ bulls_and_cows.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import random
+import sys
+
+def _come_up_number():
+    return str(random.randint(1000,9999))
+
+
+def _match_input(pc_number, user_number):
+    bulls=0
+    cows=0
+    for i, digit in enumerate(user_number):
+        if digit in pc_number:
+            bulls+=1
+        if user_number[i] == pc_number[i]:
+            cows+=1
+    return (bulls, cows)
+
+
+def _user_input():
+    user_number = input()
+    while not (len(user_number) == 4 and user_number.isdigit()):
+        if user_number.lower() in ("exit","quit","q"):
+            print("Good buy!")
+            sys.exit()
+        print("Please enter four-digit number, or 'exit' to finish game")
+        user_number = input()
+    return user_number
+
+
+if __name__ == "__main__":
+    pc_number = _come_up_number()
+    print("""-=Bulls and cows game=-
+PC comes up four-digit number. Try to quess it.
+After each step you will see response:
+    First digit - how much digits from your variant is in PC's number.
+    Second digit - how much of them is on the place.
+Good luck!""")
+    print("Please enter four-digit number, or 'exit', 'quit', 'q' to finish game")
+    result = ()
+    while result!=(4,4):
+        user_number = _user_input()
+        result = _match_input(pc_number, user_number)
+        print(str(result))
+    print("You are win!")
---
+++ sorts.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import random
+
+
+def _print_decorator(func):
+    def decorated_func(src):
+        print(func.__name__.capitalize() +" sort")
+        print("Source list " + str(src))
+        result = func(src)
+        print("Result list " + str(result) + "\n")
+    return decorated_func
+
+
+def _random_list(len, min=-10, max=10):
+    result=[]
+    for i in range(len):
+        result.append(random.randint(min,max))
+    return result
+
+
+@_print_decorator
+def buble(to_sort):
+    src = list(to_sort)
+    for n in range(1,len(src)):
+        for i in range(len(src)-n):
+            if src[i] > src[i+1]:
+               src[i],src[i+1]=src[i+1],src[i]
+    return src
+
+
+@_print_decorator
+def select(to_sort):
+    src = list(to_sort)
+    for i in range(len(src)):
+        n = i + 1
+        while n < len(src):
+            if src[n] < src[i]:
+                src[n],src[i] = src[i],src[n]
+            n+=1
+    return src
+
+
+@_print_decorator
+def insert(to_sort):
+    src = list(to_sort)
+    for i in range(1, len(src)):
+        while i > 0 and src[i - 1] > src[i]:
+            src[i - 1], src[i] = src[i], src[i - 1]
+            i-=1
+    return src
+
+
+def fib(n):
+    x = 1
+    y = 1
+    for i in range (2, n):
+        y = x + y
+        x = y - x
+    print("Fibonachi number #" + str(n) + " is " + str(y))
+
+
+if __name__=="__main__":
+    l = random_list(10)
+    buble(l)
+    select(l)
+    insert(l)
