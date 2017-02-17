1d2e3f57eccb4aab87db38ecdfb73997385f3508
Gennady Sikorskiy <g.sikorskiy@corp.mail.ru>
2017-02-17 17:37:25 +0300

    improve conditions

--- misc/classifier_test.py
+++ misc/classifier_test.py
@@ -5,17 +5,20 @@ from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
 
 
 def read_data(first_file, second_file):
-    """Reads series from two files and returns DataFrame"""
+    """Reads series from two cvs files and returns DataFrame object"""
     series1=read_csv(first_file, squeeze=True, header=None)
     series2=read_csv(second_file, squeeze=True, header=None)
-    return DataFrame({"Actual":series1,"Expected":series2}) if series1.dtype == 'float64' else DataFrame({"Actual":series2,"Expected":series1})
+    if series1.dtype == 'float64':
+        return DataFrame({"Actual":series1,"Expected":series2})
+    else:
+        return DataFrame({"Actual":series2,"Expected":series1})
 
 
 def get_metric(data_frame, metric=None):
-    """Requires DataFrame object with actual(provided by algorithm) and expected data.
+    """Requires DataFrame object with 'Actual'(provided by algorithm) and 'Expected' columns.
 metric - pass 'pr' to calculate area under precision recall curve"""
     if metric == "pr":
-        precision, recall, tresholds = precision_recall_curve(data_frame.get("Expected"), data_frame.get("Actual"))
+        precision, recall, treshold = precision_recall_curve(data_frame.get("Expected"), data_frame.get("Actual"))
         auc_prc = auc(recall, precision)
         return ("AUC_PRC", auc_prc)
     else:
@@ -25,5 +28,5 @@ metric - pass 'pr' to calculate area under precision recall curve"""
 
 if __name__=="__main__":
     data=read_data(sys.argv[1], sys.argv[2])
-    metric = sys.argv[3] if len(sys.argv) > 3 else "roc"
+    metric = "roc" if len(sys.argv) <= 3 else sys.argv[3]
     print(get_metric(data, metric)[1])
